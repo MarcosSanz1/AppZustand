bf44d12e5d4cfc75019f90399cdb6986e6c2c767
Carlos <carlos@Mac-mini-de-Innobing.local>
2022-05-02 09:23:40 +0200

    Ultimos comentarios sobre zustand

--- src/store/store.js
+++ src/store/store.js
@@ -26,7 +26,9 @@ export const usePeopleStore = create(peopleStore)
 //  - settingsStore: Este sirve para cambiar el tema a oscuro, para ello usamos una variable booleana y cuando lanzamos la función cambiaremos su valor.
 //  - peopleStore: Este sirve para añadir una nueva persona a la lista, creamos la lista y una función que recoja la nueva persona y la meta en la lista.
 
-// - devtools ¿?
+// - devtools para Local Storage
 // - persist para dejar el "estado" de forma persistente en Local Storage. De forma que aunque volvamos a renderizar este fijo.
 
 // Por último exportamos los "estados/funciones" con create para tenerlos de forma global, y poder usarlos en cualquier componente
+
+// Usar zustand para variables globales y LocalStorage o AsyncStorage para guardar de forma "fija" en Local Storage.
